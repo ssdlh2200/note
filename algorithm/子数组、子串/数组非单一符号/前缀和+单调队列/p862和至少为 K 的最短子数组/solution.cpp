b8d0d2a4ab05734dd8d36990bd5876f17e0ccf2e
ssdlh <2544310147@qq.com>
2025-11-21 19:33:03 +0800

    vault backup: 2025-11-21 19:33:03

--- algorithm/子数组、子串/数组非单一符号/前缀和+单调队列/p862和至少为 K 的最短子数组/solution.cpp
+++ algorithm/子数组、子串/数组非单一符号/前缀和+单调队列/p862和至少为 K 的最短子数组/solution.cpp
@@ -4,13 +4,15 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 class Solution {
 public:
 
-  int shortestSubarray(vector<int>& nums, int k) {
+  //暴力法
+  int shortestSubarrayV1(vector<int> &nums, int k) {
     int prefix_sum = 0;
     int res = 100001;
 
@@ -20,26 +22,45 @@ public:
       prefix_sum += nums[i];
 
       for (int j = 0; j < prefix_sums.size(); j++) {
-        if (nums[i] - prefix_sums[j] >= k) {
-          res = i - j < res ? i - j : res;
+        if (prefix_sum - prefix_sums[j] >= k) {
+          res = i-j+1  < res ? i-j+1 : res;
         }
       }
-
       prefix_sums.push_back(prefix_sum);
     }
 
     return res == 100001 ? -1 : res;
   }
 
+  //前缀和+单调队列
+  int shortestSubarray(vector<int>& nums, int k) {
+
+    deque<int> prefix_sum_deq{0};
+
+    
+    int prefix_sum;
+    
+
+    for (int i = 0; i < nums.size(); i++){
+      prefix_sum += nums[i];
+      while(!prefix_sum_deq.empty() && nums[i] <= nums[prefix_sum_deq.front()]){
+
+
+      }
+
+
+    }
+
+    return 0;
+  }
+
 };
 int main(){
 
   Solution s;
-
+  cout << "hello world" << endl;
   vector<int> nums1{2, -1, 2};
   int res1 = s.shortestSubarray(nums1, 3);
-  int nums;
-  cin >> nums;
-  cout << nums << endl;
+
   cout << res1 << endl;
 }
