8d64369a0bb23ef4eff742f85ba0f6846963a3bc
ssdlh <2544310147@qq.com>
2025-11-22 19:51:01 +0800

    vault backup: 2025-11-22 19:51:00

---
+++ algorithm/workspace/p220存在重复元素 III/solution.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++){
+            if (map.count(nums[i])){
+                if (abs(map[nums[i]] - i) <= indexDiff && )
+            }
+
+            map[nums[i]] = i;
+        }
+
+
+        return false;
+    }
+};
--- algorithm/哈希表、哈希集合/p219存在重复元素II/solution.cpp
+++ algorithm/哈希表、哈希集合/p219存在重复元素II/solution.cpp
@@ -7,22 +7,16 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int, int> map;
-
+        
         for (int i = 0; i < nums.size(); i++){
 
             if(map.count(nums[i])){
-                if (abs(map[i]-i)<=k){
+                if (abs(map[nums[i]]-i)<=k){
                     return true;
-                }               
-            } 
-
+                }     
+            }
+            map[nums[i]] = i;
         }
-
-
-
-
-
-
         return false;
     }
 };
