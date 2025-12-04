c7af2f67135b51acc13776988a6bf60c8a710f31
ssdlh <2544310147@qq.com>
2025-12-04 22:55:57 +0800

    vault backup: 2025-12-04 22:55:57

---
+++ algorithm/栈/P71 简化路径/solution.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        vector<string> stack;
+        stringstream
+
+        return "";
+    }
+};
+
+
+int main(){
+    string s = "hello world";
+
+    cout << s << endl;
+
+}
