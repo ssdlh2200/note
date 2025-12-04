bd97717a19b427e3437bbe013b66c3f6c3d35bdb
ssdlh <2544310147@qq.com>
2025-12-05 01:11:10 +0800

    2025-12-05 01:11:10

--- algorithm/栈/P71 简化路径/solution.cpp
+++ algorithm/栈/P71 简化路径/solution.cpp
@@ -12,17 +12,47 @@ public:
         
         vector<string> stack;
         string s;
+        string res;
         istringstream iss{path};
-        
-        getline(iss, s, '/');
-        return "";
+        while(getline(iss, s, '/')){
+            if ("" == s || "." == s){
+                continue;
+            } else if (".." == s){
+                if(!stack.empty()){
+                    stack.pop_back();
+                }
+            } else {
+                stack.push_back(s);
+            }
+            //cout << s << endl;
+        }
+        if(stack.empty()){
+            res.append("/");
+        } else {
+            for(string& c : stack){
+                res.append("/");
+                res.append(c);
+            }
+        }
+        return res;
     }
 };
 
-
 int main(){
-    string s = "hello world";
-
-    cout << s << endl;
-
+    string s1 = "/dada/dada/dada/dada/../dada/da/./da//da////da";
+    string s2 = "/";
+    string s3 = "///////";
+    string s4 = "/../";
+    string s5 = "/........../.../ad";
+    Solution sol;
+    string res1 = sol.simplifyPath(s1);
+    string res2 = sol.simplifyPath(s2);
+    string res3 = sol.simplifyPath(s3);
+    string res4 = sol.simplifyPath(s4);
+    string res5 = sol.simplifyPath(s5);
+    cout << res1 << endl;
+    cout << res2 << endl;
+    cout << res3 << endl;
+    cout << res4 << endl;
+    cout << res5 << endl;
 }
